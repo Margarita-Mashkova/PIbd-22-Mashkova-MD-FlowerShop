42c7635f3c8b0bc732acb8ef18cc9d8c5fef13f1
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-02-03 03:34:31 +0400

    comm

--- FlowerShop/FlowerShopConracts/ViewModels/OrderViewModel.cs
+++ FlowerShop/FlowerShopConracts/ViewModels/OrderViewModel.cs
@@ -11,17 +11,23 @@ namespace FlowerShopConracts.ViewModels
     public class OrderViewModel
     {
         public int Id { get; set; }
-        public int ProductId { get; set; }
+        public int FlowerId { get; set; }
+
         [DisplayName("Букет")]
-        public string ProductName { get; set; }
+        public string FlowerName { get; set; }
+
         [DisplayName("Количество")]
         public int Count { get; set; }
+
         [DisplayName("Сумма")]
         public decimal Sum { get; set; }
+
         [DisplayName("Статус")]
         public string Status { get; set; }
+
         [DisplayName("Дата создания")]
         public DateTime DateCreate { get; set; }
+
         [DisplayName("Дата выполнения")]
         public DateTime? DateImplement { get; set; }
     }
