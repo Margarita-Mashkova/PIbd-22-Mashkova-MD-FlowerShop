16b8ca93a1ace251ca18d13c40b7886e8220f6ba
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-02-03 00:30:30 +0400

    comm

---
+++ FlowerShop/FlowerShopConracts/BindingModels/CreateOrderBindingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShopConracts.BindingModels
+{
+    /// Данные от клиента, для создания заказа
+    public class CreateOrderBindingModel
+    {
+        public int FlowerId { get; set; }
+        public int Count { get; set; }
+        public decimal Sum { get; set; }
+    }
+}
