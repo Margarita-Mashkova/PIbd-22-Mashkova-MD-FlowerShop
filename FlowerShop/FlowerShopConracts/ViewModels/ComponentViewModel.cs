d5993fd8febce27347b59525c0d86980f9b0f02d
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-02-03 00:50:47 +0400

    comm

---
+++ FlowerShop/FlowerShopConracts/ViewModels/ComponentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace FlowerShopConracts.ViewModels
+{
+    /// Компонент, требуемый для изготовления букета
+    public class ComponentViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Название компонента")]
+        public string ComponentName { get; set; }
+    }
+}
--- FlowerShop/FlowerShopConracts/ViewModels/FlowerViewModel.cs
+++ FlowerShop/FlowerShopConracts/ViewModels/FlowerViewModel.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
-namespace FlowerShopConracts.BindingModels
+namespace FlowerShopConracts.ViewModels
 {
     /// Букет, собираемый в цветочной лавке
-    public class FlowerBindingModel
+    public class FlowerViewModel
     {
-        public int? Id { get; set; }
-        public string FlowerName { get; set; }
+        public int Id { get; set; }
+        [DisplayName("Название букета")]
+        public string ProductName { get; set; }
+        [DisplayName("Цена")]
         public decimal Price { get; set; }
         public Dictionary<int, (string, int)> FlowerComponents { get; set; }
     }
