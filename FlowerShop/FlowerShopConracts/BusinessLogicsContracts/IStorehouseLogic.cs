01214fb3c5af9c352f11d93fc492d0ad6b9ab336
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-02-17 08:19:35 +0400

    com

---
+++ FlowerShop/FlowerShopConracts/BusinessLogicsContracts/IStorehouseLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowerShopConracts.BindingModels;
+using FlowerShopConracts.ViewModels;
+
+namespace FlowerShopConracts.BusinessLogicsContracts
+{
+    public interface IStorehouseLogic
+    {
+       
+    }
+}
