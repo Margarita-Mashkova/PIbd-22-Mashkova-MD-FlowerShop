95d897c3d943f148a283916692a307dec5e95e3b
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-03-13 10:37:59 +0400

    commit

---
+++ FlowerShop/FlowerShopBusinessLogic/BusinessLogics/ReportLogic.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowerShopBusinessLogic.OfficePackage;
+using FlowerShopBusinessLogic.OfficePackage.HelperModels;
+using FlowerShopConracts.BindingModels;
+using FlowerShopConracts.BusinessLogicsContracts;
+using FlowerShopConracts.StoragesContracts;
+using FlowerShopConracts.ViewModels;
+
+
+namespace FlowerShopBusinessLogic.BusinessLogics
+{
+    public class ReportLogic : IReportLogic
+    {
+        private readonly IComponentStorage _componentStorage;
+        private readonly IFlowerStorage _flowerStorage;
+        private readonly IOrderStorage _orderStorage;
+        private readonly AbstractSaveToExcel _saveToExcel;
+        private readonly AbstractSaveToWord _saveToWord;
+        private readonly AbstractSaveToPdf _saveToPdf;
+        public ReportLogic(IFlowerStorage flowerStorage, IComponentStorage componentStorage, IOrderStorage orderStorage,
+                            AbstractSaveToExcel saveToExcel, AbstractSaveToWord saveToWord, AbstractSaveToPdf saveToPdf)
+        {
+            _flowerStorage = flowerStorage;
+            _componentStorage = componentStorage;
+            _orderStorage = orderStorage;
+            _saveToExcel = saveToExcel;
+            _saveToWord = saveToWord;
+            _saveToPdf = saveToPdf;
+        }
+
+        // Получение списка компонент с указанием, в каких изделиях используются
+        public List<ReportFlowerComponentViewModel> GetFlowerComponent()
+        {
+            var components = _componentStorage.GetFullList();
+            var flowers = _flowerStorage.GetFullList();
+            var list = new List<ReportFlowerComponentViewModel>();
+            foreach (var component in components)
+            {
+                var record = new ReportFlowerComponentViewModel
+                {
+                    ComponentName = component.ComponentName,
+                    Flowers = new List<Tuple<string, int>>(),
+                    TotalCount = 0
+                };
+                foreach (var flower in flowers)
+                {
+                    if (flower.FlowerComponents.ContainsKey(component.Id))
+                    {
+                        record.Flowers.Add(new Tuple<string, int>(flower.FlowerName, flower.FlowerComponents[component.Id].Item2));
+                        record.TotalCount += flower.FlowerComponents[component.Id].Item2;
+                    }
+                }
+                list.Add(record);
+            }
+            return list;
+        }
+
+        // Получение списка заказов за определенный период
+        public List<ReportOrdersViewModel> GetOrders(ReportBindingModel model)
+        {
+            return _orderStorage.GetFilteredList(new OrderBindingModel
+            {
+                DateFrom = model.DateFrom,
+                DateTo = model.DateTo
+            })
+            .Select(x => new ReportOrdersViewModel
+            {
+                DateCreate = x.DateCreate,
+                FlowerName = x.FlowerName,
+                Count = x.Count,
+                Sum = x.Sum,
+                Status = x.Status
+            })
+           .ToList();
+        }
+
+        // Сохранение компонент в файл-Word
+        public void SaveComponentsToWordFile(ReportBindingModel model)
+        {
+            _saveToWord.CreateDoc(new WordInfo
+            {
+                FileName = model.FileName,
+                Title = "Список компонент",
+                Components = _componentStorage.GetFullList()
+            });
+        }
+
+        // Сохранение компонент с указаеним продуктов в файл-Excel
+        public void SaveFlowerComponentToExcelFile(ReportBindingModel model)
+        {
+            _saveToExcel.CreateReport(new ExcelInfo
+            {
+                FileName = model.FileName,
+                Title = "Список компонент",
+                FlowerComponents = GetFlowerComponent()
+            });
+        }
+
+        // Сохранение заказов в файл-Pdf
+        public void SaveOrdersToPdfFile(ReportBindingModel model)
+        {
+            _saveToPdf.CreateDoc(new PdfInfo
+            {
+                FileName = model.FileName,
+                Title = "Список заказов",
+                DateFrom = model.DateFrom.Value,
+                DateTo = model.DateTo.Value,
+                Orders = GetOrders(model)
+            });
+        }
+    }
+}
